e079142504852a8d88bd6ee8ba3003b4af7f2c00
Higor Souza <higor.moriyama@gmail.com>
2022-08-02 12:20:46 -0300

    Beginner Problems Start

---
+++ 1 - Beginner Problems/1_HelloWorld.js
@@ -0,0 +1,6 @@
+/*
+* In this first problem all you have to do is print the message "Hello World" on the screen.
+* Neste primeiro problema, tudo que deve fazer é imprimir a mensagem "Hello World" na tela.
+*/
+
+console.log("Hello World!");
